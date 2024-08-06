ad0461d077812611cee8ea8c8a0a152f4f11ea5c
joaoEGiovanne <joaogiovanneetec@gmail.com>
2024-08-06 12:41:08 -0300

    subindo aula

---
+++ ExemploHttp/View/PostsView.xaml.cs
@@ -0,0 +1,12 @@
+using ExemploHttp.ViewModels;
+namespace ExemploHttp.View;
+
+public partial class PostsView : ContentPage
+{
+	public PostsView()
+	{
+		BindingContext = new PostsViewModels();
+		InitializeComponent();
+
+	}
+}
---
+++ ExemploHttp/ViewModels/PostsViewModels.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ExemploHttp.Models;
+using ExemploHttp.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExemploHttp.Services;
+using System.Windows.Input;
+
+namespace ExemploHttp.ViewModels
+{
+    public partial class PostsViewModels : ObservableObject
+    {
+        [ObservableProperty]
+
+        ObservableCollection<Post> posts;
+
+        public ICommand getPostsCommand;
+
+        public PostsViewModels()
+        {
+            getPostsCommand = new Command(getPosts);
+        }
+
+        public async void getPosts()
+        {
+            RestService restService = new RestService();
+            Posts = await restService.getPostAsync();
+        }
+    }
+}
